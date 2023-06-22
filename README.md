8c68ff954bd2e1ceebec4d9d7f27197f19f7ee46
Robert Kroeger <rjkroege@liqui.org>
2023-06-22 06:21:37 -0400

    Interim check-in
    Start of building the indexer tool.

---
+++ README.md
@@ -0,0 +1,12 @@
+# Columns
+
+* filehash
+* prefix
+* snapshot hash
+* path
+
+# Constraints
+
+* html escape the textual fields
+* space to separate
+* newline terminated lines
---
+++ main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"encoding/json"
+	"io"
+	"log"
+	"os"
+	"os/exec"
+
+	"github.com/kopia/kopia/cli"
+)
+
+func main() {
+	log.Println("foo")
+
+	// Get the list of snapshots.
+	cmd := exec.Command("kopia", "snapshot", "list", "-a", "--json")
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := cmd.Start(); err != nil {
+		log.Fatal(err)
+	}
+
+	dec := json.NewDecoder(stdout)
+	dec.DisallowUnknownFields()
+
+	var manifests []cli.SnapshotManifest
+
+	if err := dec.Decode(&manifests); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := cmd.Wait(); err != nil {
+		log.Fatal(err)
+	}
+
+	for _, k := range manifests {
+		log.Println(k)
+
+		// these can run in parallel
+		// is there a benefit?
+		listSnapshot(string(k.ID))
+	}
+}
+
+func listSnapshot(snapshotid string) {
+	// Get the list.
+
+	cmd := exec.Command("kopia", "list", "-r", "-o", snapshotid)
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := cmd.Start(); err != nil {
+		log.Fatal(err)
+	}
+
+	// Copy from the stdout...
+	// TODO(rjk): parse the listing.
+	io.Copy(os.Stdout, stdout)
+
+	if err := cmd.Wait(); err != nil {
+		log.Fatal(err)
+	}
+
+}
