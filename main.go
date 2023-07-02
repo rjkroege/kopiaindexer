bf6dcf16de3001f90135b4c581d8d9bac9a74585
Alex Nicolaou <alex@stockgamblers.com>
2023-07-02 12:41:35 -0400

    Terrible hack to demonstrate the usage I'd like.
    I couldn't figure out how to do this properly but here is code
    that makes kopiaindexer do what I want, which is:
    
    ./kopiaindexer --manifest=MANIFEST_ID_HERE
    
    will output the dump of just the snapshot from the given manifest
    ID and stop.

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"log"
 	"os"
 	"os/exec"
@@ -12,8 +13,48 @@ import (
 func main() {
 	log.Println("foo")
 
+	var kID = "";
+	flag.StringVar(&kID, "manifest", "", "Specify a manifest id");
+	flag.Parse();
+
+
 	// Get the list of snapshots.
 	cmd := exec.Command("kopia", "snapshot", "list", "-a", "--json")
+	if (len(kID) > 0) {
+		log.Println("Processing single manifest", kID)
+		//cmd = exec.Command("kopia", "manifest", "show", kID)
+		cmd = exec.Command("catmanifest", kID)
+
+		stdout, err := cmd.StdoutPipe()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if err := cmd.Start(); err != nil {
+			log.Fatal(err)
+		}
+
+		dec := json.NewDecoder(stdout)
+		dec.DisallowUnknownFields()
+
+		var manifest cli.SnapshotManifest
+		if err := dec.Decode(&manifest); err != nil {
+			log.Fatal(err)
+		}
+
+		if err := cmd.Wait(); err != nil {
+			log.Fatal(err)
+		}
+
+		k := manifest;
+		log.Println(k.ID, k.Source.String())
+
+		// TODO(rjk): This could be parallelized.
+		escapedsource := pathUrlEscape(k.Source.String())
+		log.Println("List snapshot", k.RootEntry.ObjectID);
+		listSnapshot(kID, escapedsource)
+		return;
+	}
 	stdout, err := cmd.StdoutPipe()
 	if err != nil {
 		log.Fatal(err)
